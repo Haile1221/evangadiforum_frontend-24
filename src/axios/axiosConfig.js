8ee84fb4dcc4a44dcaa4abb045535fe70cb1b286
Haile <61838024+Haile1221@users.noreply.github.com>
2024-06-25 18:28:41 +0300

    frontend updated

--- src/axios/axiosConfig.js
+++ src/axios/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from "axios";
 const axiosBase = axios.create({
   // baseURL: "http://localhost:5000/api",
 
-  baseURL:
-    "https://vercel.com/hailes-projects-b71b915a/evangadiforum-backend-deployement-24",
+  baseURL: "https://evangadiforum-backend-deployement-24.onrender.com/",
+
   headers: {
     "Content-Type": "application/json",
   },
